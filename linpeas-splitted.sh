49d91e6d24bc8e06d02e855a3826c7b7da4bc032
ezekieltan <39935586+ezekieltan@users.noreply.github.com>
2025-04-04 16:59:41 +0800

    Update linpeas-splitted.sh

--- linpeas-splitted.sh
+++ linpeas-splitted.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# Default value for input_script
 default_filename="lp.sh"
-
 linpeas_filename="${1:-$default_filename}"
+random_string="${2:-$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20)}"
 
 echo "Downloading linpeas"
 wget -q https://github.com/peass-ng/PEASS-ng/releases/latest/download/linpeas.sh -O "$linpeas_filename"
@@ -13,8 +12,10 @@ wget -q https://raw.githubusercontent.com/ezekieltan/linpeas-splitted/refs/heads
 
 echo "Splitting..."
 chmod +x linpeas-splitter.sh
+./linpeas-splitter.sh "$linpeas_filename" "lp_output" "$random_string"
 
-./linpeas-splitter.sh "$linpeas_filename" "lp_output"
-echo "Complete"
+echo "Complete, cleaning up..."
 rm -rf "$linpeas_filename"
 rm -rf linpeas-splitter.sh
+
+echo "Done!"
